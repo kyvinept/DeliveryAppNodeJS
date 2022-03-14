e0911c4e001f1a48f52f3c620360fd481c1c330a
Silchenko <kyvinept@gmail.com>
2022-03-14 10:42:58 +0200

    remove logs

--- src/helpers/hash.ts
+++ src/helpers/hash.ts
@@ -1,7 +1,6 @@
 import crypto from 'crypto';
 
 export const hash = (string: string): string => {
-  console.log('process.env.SALT_VALUE', process.env.SALT_VALUE);
   const hash = crypto
     .pbkdf2Sync(string, process.env.SALT_VALUE, 1000, 64, 'sha512')
     .toString('hex');
@@ -9,8 +8,6 @@ export const hash = (string: string): string => {
 };
 
 export const compareStrings = (string1: string, string2: string) => {
-  console.log(string1, string2);
   const hashedString = hash(string1);
-  console.log(hashedString);
   return hashedString == string2;
 };
