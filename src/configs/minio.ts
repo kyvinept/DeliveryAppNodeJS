c9b4ac405849d97007d014c72ebbce9f7575dd39
Silchenko <kyvinept@gmail.com>
2022-03-14 10:00:57 +0200

    update files

--- src/configs/minio.ts
+++ src/configs/minio.ts
@@ -3,9 +3,21 @@ import * as Minio from 'minio';
 const client = new Minio.Client({
   endPoint: process.env.MINIO_END_POINT,
   port: parseInt(process.env.MINIO_PORT),
-  useSSL: false,
+  useSSL: true,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 });
+
+const createBucketIfNeeded = async () => {
+  try {
+    const bucketList = await client.listBuckets();
+    console.log('bucketList', bucketList);
+    await client.makeBucket('images', 'us-east-1');
+  } catch (e) {
+    console.log('createBucketIfNeeded error', e);
+  }
+};
+
+createBucketIfNeeded();
 
 export default client;
